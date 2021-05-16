70ecd85ba8f8fa5214347e971b3436eb9a4ad12e
Dmitry Kuznetsov <kuznetcov.rabota@mail.ru>
2021-05-16 18:23:51 +0300

    Доделан основной тест

--- HomeWorkSelenium/src/test/java/HomeWorkTest.java
+++ HomeWorkSelenium/src/test/java/HomeWorkTest.java
@@ -25,6 +25,8 @@ public class HomeWorkTest {
         js = (JavascriptExecutor) driver;
         vars = new HashMap<String, Object>();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://www.avito.ru/");
+        driver.manage().window().setSize(new Dimension(1382, 744));
     }
 
     @AfterEach
@@ -33,65 +35,26 @@ public class HomeWorkTest {
     }
 
     @Test
-    public void testHW() {
-        driver.get("https://www.avito.ru/penza");
-        driver.manage().window().setSize(new Dimension(1382, 744));
+    public void testHW() throws InterruptedException {
         driver.findElement(By.id("category")).click();
         {
             WebElement dropdown = driver.findElement(By.id("category"));
-            dropdown.findElement(By.xpath("//option[. = 'Любая категория']")).click();
+            dropdown.findElement(By.xpath("//option[. = 'Оргтехника и расходники']")).click();
         }
-        driver.findElement(By.id("category")).click();
         driver.findElement(By.id("search")).click();
         driver.findElement(By.id("search")).sendKeys("Принтер");
         driver.findElement(By.cssSelector(".main-location-3j9by > .main-text-2PaZG")).click();
         driver.findElement(By.cssSelector(".suggest-input-3p8yi")).sendKeys("Владивосток");
-        driver.findElement(By.cssSelector(".suggest-suggest_regions-qQeGI")).click();
-        {
-            WebElement element = driver.findElement(By.cssSelector(".button-size-m-7jtw4"));
-            Actions builder = new Actions(driver);
-            builder.moveToElement(element).clickAndHold().perform();
-        }
-        {
-            WebElement element = driver.findElement(By.cssSelector(".button-size-m-7jtw4 > .button-textBox-Row9K"));
-            Actions builder = new Actions(driver);
-            builder.moveToElement(element).release().perform();
-        }
+        Thread.sleep(1000);// Задержка нужна, т.к. при написании города список не успевает обновиться
+            // под него и выбирает город по геопозоции
+        driver.findElement(By.xpath("//li[@data-marker=\"suggest(0)\"]")).click();
         driver.findElement(By.cssSelector(".button-size-m-7jtw4")).click();
-        js.executeScript("window.scrollTo(0,65)");
-        js.executeScript("window.scrollTo(0,300)");
         js.executeScript("window.scrollTo(0,485)");
-        driver.findElement(By.cssSelector(".column-root-N_0Ue > div > .checkbox-checkbox-7igZ6 > .checkbox-label-3AzRS")).click();
-        {
-            WebElement element = driver.findElement(By.cssSelector(".applyButton-root-1KoTq > .button-button-2Fo5k"));
-            Actions builder = new Actions(driver);
-            builder.moveToElement(element).clickAndHold().perform();
-        }
-        {
-            WebElement element = driver.findElement(By.cssSelector(".button-primary-1RhOG > .button-textBox-Row9K"));
-            Actions builder = new Actions(driver);
-            builder.moveToElement(element).release().perform();
-        }
-        driver.findElement(By.cssSelector(".applyButton-root-1KoTq > .button-button-2Fo5k")).click();
-        {
-            WebElement element = driver.findElement(By.cssSelector(".button-primary-1RhOG > .button-textBox-Row9K"));
-            Actions builder = new Actions(driver);
-            builder.moveToElement(element).perform();
-        }
-        js.executeScript("window.scrollTo(0,141)");
-        driver.findElement(By.cssSelector(".sort-select-3QxXG > .select-select-3CHiM")).click();
+        driver.findElement(By.xpath("//div[@data-marker=\"delivery-filter/container\"]")).click();
+        driver.findElement(By.cssSelector(".applyButton-root-1KoTq > .button-size-s-3-rn6")).click();
         {
             WebElement dropdown = driver.findElement(By.cssSelector(".sort-select-3QxXG > .select-select-3CHiM"));
-            dropdown.findElement(By.xpath("//option[. = 'По умолчанию']")).click();
+            dropdown.findElement(By.xpath("//option[. = 'Дороже']")).click();
         }
-        driver.findElement(By.cssSelector(".sort-select-3QxXG > .select-select-3CHiM")).click();
-        js.executeScript("window.scrollTo(0,142)");
-        {
-            WebElement element = driver.findElement(By.cssSelector("#i2075025699 .link-link-39EVK > .title-root-395AQ"));
-            Actions builder = new Actions(driver);
-            builder.moveToElement(element).perform();
-        }
-        js.executeScript("window.scrollTo(0,510)");
-        js.executeScript("window.scrollTo(0,200)");
     }
 }
