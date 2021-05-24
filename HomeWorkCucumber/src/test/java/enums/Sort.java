e452c1a1a9d66cfd429881273c6be4cc18b19332
Dmitry Kuznetsov <kuznetcov.rabota@mail.ru>
2021-05-24 17:30:20 +0300

    Наполнил списки

--- HomeWorkCucumber/src/test/java/enums/Sort.java
+++ HomeWorkCucumber/src/test/java/enums/Sort.java
@@ -1,4 +1,20 @@
 package enums;
 
 public enum Sort {
+
+    ПоУмолчанию("По умолчанию"),
+    Дешевле("Дешевле"),
+    Дороже("Дороже"),
+    ПоДате(" По дате");
+
+    public String value;
+
+    public String getValue() {
+        return value;
+    }
+
+
+    Sort (String value) {
+        this.value = value;
+    }
 }
